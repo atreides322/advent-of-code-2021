a12138e7418dd2a804fdd08dd4006ecc21d243ec
Joe Atkins <joe.atkins@objectpartners.com>
2021-12-01 16:48:56 -0600

    Day 01 - Part 01

---
+++ 01/day01.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import sys
+from functools import reduce
+
+def main():
+
+    if len(sys.argv) != 2:
+        sys.exit(f"Usage: {sys.argv[0]} input_file")
+
+    with open(sys.argv[1]) as input:
+        numbers = [ int(x) for x in input.readlines()]
+
+    if not numbers:
+        return
+
+    count = 0
+    previous = numbers[0]
+
+    for number in numbers[1:]:
+        if number > previous:
+            count+=1
+        previous = number
+
+    print(count)
+
+
+if __name__ == "__main__":
+    main()

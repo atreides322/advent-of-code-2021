78317b856a0408ff2e0f52683ed156b91e96adbb
Joe Atkins <joe.atkins@objectpartners.com>
2021-12-07 22:18:54 -0600

    Day 07 - Part 01

---
+++ 07/day07.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+def main():
+
+    if len(sys.argv) != 2:
+        sys.exit(f"Usage: {sys.argv[0]} input_file")
+
+    with open(sys.argv[1]) as input:
+        positions = [int(x) for x in input.readline().split(',')]
+
+    first = min(positions)
+    last = max(positions)
+
+    best = sys.maxsize
+    target = None
+
+    for destination in range(first, last):
+        fuel = 0
+        for sub in positions:
+            fuel += abs(sub - destination)
+
+        if fuel < best:
+            best = fuel
+            target = destination
+
+    print('Move subs to', target, 'using', best, 'fuel.')
+
+
+if __name__ == "__main__":
+    main()

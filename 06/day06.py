998b9a9ee367966b5d67b5990823d2701616d92f
Joe Atkins <joe.atkins@objectpartners.com>
2021-12-07 19:35:53 -0600

    Day 06 - Part 02

--- 06/day06.py
+++ 06/day06.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+from itertools import groupby
 
 
 def main():
@@ -16,21 +17,34 @@ def main():
     with open(args.input) as input:
         fish = [int(x) for x in input.readline().split(',')]
 
+    fish = {k: len(list(v)) for k, v in groupby(sorted(fish))}
+
     if args.verbosity:
         print('Initial State:', fish_str(fish, args))
 
     for day in range(args.days):
-        new_fish = [8 for x in fish if x == 0]
-        fish = [x-1 if x > 0 else 6 for x in fish]
-        fish.extend(new_fish)
+        new_fish = fish.get(0, 0)
+        fish = {(k-1): v for k, v in fish.items() if k > 0}
+
+        if new_fish:
+            fish[6] = fish.get(6, 0) + new_fish
+            fish[8] = new_fish
+
         if args.verbosity:
             print(f'Day {day+1}: {fish_str(fish, args)}')
 
-    print(len(fish))
+    print(count_fish(fish))
 
 
 def fish_str(fish, args):
-    return ', '.join([str(x) for x in fish]) if args.verbosity > 1 else len(fish)
+    if args.verbosity > 1:
+        return ', '.join([f'{v}({k} days)' for k, v in fish.items()])
+    else:
+        return str(count_fish(fish))
+
+
+def count_fish(fish):
+    return sum([v for k, v in fish.items()])
 
 
 if __name__ == '__main__':

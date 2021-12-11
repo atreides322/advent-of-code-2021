ac272059f8892dc7e76cbafd4ffd5493a0499f15
Joe Atkins <joe.atkins@objectpartners.com>
2021-12-11 00:21:59 -0600

    Day 09 - Part 02

--- 09/day09.py
+++ 09/day09.py
@@ -12,6 +12,12 @@ def main():
         topography = [[int(x) for x in line.strip()]
                       for line in input.readlines()]
 
+    calc_risk(topography)
+    calc_basins(topography)
+
+
+def calc_risk(topography):
+
     max_i = len(topography)
     max_j = len(topography[0])
 
@@ -37,6 +43,41 @@ def main():
                 risk += 1 + value
 
     print('Risk:', risk)
+
+
+def calc_basins(topography):
+
+    max_i = len(topography)
+    max_j = len(topography[0])
+
+    visited = [[False] * max_j for _ in range(max_i)]
+
+    basins = []
+
+    for i, row in enumerate(topography):
+        for j, value in enumerate(row):
+            basin = find_basin(i, j, max_i, max_j, topography, visited)
+            basins.append(basin)
+
+    area = 1
+    for x in sorted(basins)[-3:]:
+        area *= x
+
+    print('Area:', area)
+
+
+def find_basin(i, j, max_i, max_j, topography, visited):
+    if i < 0 or j < 0 or i >= max_i or j >= max_j \
+            or topography[i][j] == 9 or visited[i][j]:
+        return 0
+
+    visited[i][j] = True
+
+    return (1
+            + find_basin(i - 1, j, max_i, max_j, topography, visited)
+            + find_basin(i, j - 1, max_i, max_j, topography, visited)
+            + find_basin(i + 1, j, max_i, max_j, topography, visited)
+            + find_basin(i, j + 1, max_i, max_j, topography, visited))
 
 
 if __name__ == "__main__":
